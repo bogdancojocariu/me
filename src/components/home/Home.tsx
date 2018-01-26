ee933e53f852e4c8284f90b904375d3956e278b3
Bogdan Cojocariu <bogdan.cojocariu@P6604.svdomain1.softvision.ro>
2018-01-26 17:11:46 +0200

    added Homepage

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, my name is Bogdan and I am a front-end developer</h1>
+                <h2>Welcome to my homepage</h2>
+            </div>
+        );
+    }
+}
