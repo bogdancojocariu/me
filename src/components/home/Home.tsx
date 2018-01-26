94c97bc83f78de62985a8a565e599fbe2a50c255
Bogdan Cojocariu <bogdan.cojocariu@P6604.svdomain1.softvision.ro>
2018-01-26 18:10:41 +0200

    typings

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
+import Typing from 'react-typist';
 
 export class Home extends React.Component {
     render() {
+        const social = ['facebook', 'linkedin', 'tinder'];
+        const getSocial = social.map( s => {
+            return (
+                <span key={s}>
+                    <span>{s}</span>
+                    <Typing.Backspace count={s.length} delay={500} />
+                </span>
+            );
+        });
+
         return (
             <div>
                 <h1>Hello, my name is Bogdan and I am a front-end developer</h1>
                 <h2>Welcome to my homepage</h2>
+                <div className="social">
+                    <Typing>
+                        {getSocial}
+                        <span>twitter</span>
+                    </Typing>
+                </div>
             </div>
         );
     }
