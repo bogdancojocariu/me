3d38e8d2f316ee3c4419d32755ac037a4bd3c3f7
Bogdan Cojocariu <bogdancojocariu24@gmail.com>
2018-01-28 20:45:37 +0200

    added font, new design

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import * as React from 'react';
 import { Home } from './components/home/Home';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
                 <Home />
             </div>
         );
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,35 @@
 import * as React from 'react';
-import Typing from 'react-typist';
+import Typing from 'react-typing-animation';
+import styled from 'styled-components';
+import FA from 'react-fontawesome';
+
+const right = require('./right_side.jpg');
+
+const Wrapper = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+`;
+const Title = styled.div`
+    margin-top: 30%;
+`;
+
+const LeftSide = styled.div`
+    float: left;
+    width: 60%;
+    height: 100%;
+`;
+const RightSide = styled.div`
+    width: 40%;
+    height: 100%;
+    background-image: url(${right});
+    background-size: cover;
+`;
+const StyledSocial = styled.a`
+    margin: 0 20px;
+    color: black;
+`;
 
 export class Home extends React.Component {
     render() {
@@ -13,17 +43,39 @@ export class Home extends React.Component {
             );
         });
 
+        const socialLinks = [
+            {site: 'facebook', icon: 'facebook-square', link: 'https://www.facebook.com/cbogdan'},
+            {site: 'instagram', icon: 'instagram', link: 'https://www.instagram.com/bogdan.cojocariu/'},
+            {site: 'linkedin', icon: 'linkedin-square ', link: 'https://www.linkedin.com/in/bcojocariu/'}
+        ];
+
+        const generateSocialLinks = socialLinks.map(s => {
+            return (
+                <StyledSocial href={s.link} key={s.site} className="fa-stack fa-lg">
+                    <FA name={s.icon} size="2x" />
+                </StyledSocial>
+            );
+        });
+
         return (
-            <div>
-                <h1>Hello, my name is Bogdan and I am a frontend developer</h1>
-                <h2>Welcome to my homepage</h2>
-                <div className="social">
-                    <Typing>
-                        {getSocial}
-                        <span>twitter</span>
-                    </Typing>
-                </div>
-            </div>
+            <Wrapper>
+                <LeftSide>
+                    <Title>
+                        <h1>Hello, I am Bogdan and I am a frontend developer</h1>
+                        <h2>Welcome to my homepage</h2>
+                        <div className="social">
+                            <Typing>
+                                {getSocial}
+                                <span>twitter</span>
+                            </Typing>
+                        </div>
+                    </Title>
+                    <div>
+                        {generateSocialLinks}
+                    </div>
+                </LeftSide>
+                <RightSide />
+            </Wrapper>
         );
     }
 }
