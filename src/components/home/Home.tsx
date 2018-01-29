f4a013beebec35f220d95599306e26548f776639
Bogdan Cojocariu <bogdancojocariu24@gmail.com>
2018-01-29 23:06:04 +0200

    fixes

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Typing from 'react-typing-animation';
+import Typist from 'react-typist';
 import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 import FA from 'react-fontawesome';
@@ -18,7 +18,7 @@ const Title = styled.div`
 const MobileHeaderImage = styled.img`
     border-radius: 20px;
     width: 40%;
-    margin: 0 auto;
+    margin: 20px auto;
     ${({theme}) => breakpoint('lg', theme.breakpoints)`
         display: none;
     `}
@@ -27,12 +27,13 @@ const LeftSide = styled.div`
     box-sizing: border-box;
     padding: 20px;
     width: 100%;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
     ${({theme}) => breakpoint('lg', theme.breakpoints)`
         display: flex;
+        flex-direction: column;
         float: left;
-        width: calc(100% - 650px);
+        width: calc(100% - 580px);
         height: 100%;
     `}
 `;
@@ -42,7 +43,7 @@ const RightSide = styled.img`
         position: absolute;
         right: 0;
         display: block;
-        width: 650px;
+        width: 580px;
         height: 100%;
     `}
 `;
@@ -50,15 +51,24 @@ const StyledSocial = styled.a`
     margin: 0 20px;
     color: black;
 `;
+const StyledSpan = styled.span`
+    color: blue;
+`;
+const StyledTypist = styled(Typist)`
+    display: inline;
+`;
+const SocialWrapper = styled.div`
+    width: 100%;
+`;
 
 export class Home extends React.Component {
     render() {
-        const social = ['facebook', 'linkedin', 'pornhub', 'tinder'];
+        const social = ['frontend developer', 'runner', 'swimmer'];
         const getSocial = social.map( s => {
             return (
                 <span key={s}>
-                    <span>{s}</span>
-                    <Typing.Backspace count={s.length} delay={500} />
+                    <span> {s}</span>
+                    <Typist.Backspace count={s.length} delay={500} />
                 </span>
             );
         });
@@ -77,23 +87,27 @@ export class Home extends React.Component {
             );
         });
 
+        const cursor = {
+            hideWhenDone: true
+        };
+
         return (
             <Wrapper>
                 <LeftSide>
                     <Title>
                         <MobileHeaderImage src={left} />
-                        <h1>Hello, I am Bogdan and I am a frontend developer</h1>
-                        <h2>Welcome to my homepage</h2>
-                        <div className="social">
-                            <Typing>
-                                {getSocial}
-                                <span>twitter</span>
-                            </Typing>
-                        </div>
-                        <div>
-                            {generateSocialLinks}
-                        </div>
+                        <h1>Hello, I am Bogdan and I am a 
+                             <StyledSpan>
+                                <StyledTypist cursor={cursor}>
+                                    {getSocial}
+                                    <span> cyclist</span>
+                                </StyledTypist>
+                            </StyledSpan>
+                        </h1>
                     </Title>
+                    <SocialWrapper>
+                        {generateSocialLinks}
+                    </SocialWrapper>
                 </LeftSide>
                 <RightSide src={right}/>
             </Wrapper>
